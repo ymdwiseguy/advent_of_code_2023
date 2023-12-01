694dfef26f31ba86e608bb75703fc08806681b36
Matthias Diekmann <9092617+ymdwiseguy@users.noreply.github.com>
2023-12-01 15:52:52 +0100

    somehow solved part 2 (by overwriting part 1)

--- src/main/day01/Day01.kt
+++ src/main/day01/Day01.kt
@@ -1,17 +1,66 @@
 package day01
 
+import println
 import readInput
 
 fun main() {
-    println("Part 1: ${Day01().part1(Day01().input)}")
     println("Part 2: ${Day01().part2(Day01().input)}")
 }
 
 class Day01 {
     val input = readInput("resources/day01/data")
     val testInput = readInput("resources/day01/data_test")
+    val allNumbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 
-    fun part1(input: List<String>): Int = input.size
+    fun part2(input: List<String>): Int = input.sumOf {
+        val rowValue = getCalibrationValue(it) ?: 0
+        rowValue.println()
+        rowValue
+    }
 
-    fun part2(input: List<String>): Int = input.size
+    fun getAllNumbers(input: String): String {
+        val indexedNumbers = buildMap {
+            input.forEachIndexed { index, char ->
+                if (char.isDigit()) {
+                    put(index, char.digitToInt())
+                }
+            }
+            allNumbers.forEach { numberString ->
+                val found = input.indexOf(numberString)
+                if (found != -1) {
+                    stringToNumber(numberString)?.let {
+                        put(found, it)
+                    }
+                }
+                val foundLast = input.lastIndexOf(numberString)
+                if (foundLast != -1) {
+                    stringToNumber(numberString)?.let {
+                        put(foundLast, it)
+                    }
+                }
+            }
+        }
+
+        return indexedNumbers.toSortedMap().values.joinToString("")
+    }
+
+    fun getCalibrationValue(input: String): Int? {
+        val onlyNumbers = getAllNumbers(input)
+        return if (onlyNumbers.isNotEmpty())
+            "${onlyNumbers.first()}${onlyNumbers.last()}".toIntOrNull()
+        else 0
+    }
+
+    fun stringToNumber(string: String): Int? = when (string) {
+        "one" -> 1
+        "two" -> 2
+        "three" -> 3
+        "four" -> 4
+        "five" -> 5
+        "six" -> 6
+        "seven" -> 7
+        "eight" -> 8
+        "nine" -> 9
+        else -> null
+    }
 }
--- src/test/day01/Day01_test.kt
+++ src/test/day01/Day01_test.kt
@@ -15,7 +15,41 @@ class Day01Test {
     }
 
     @Test
-    fun `failing dummy test`() {
-         input.size shouldBe 1
+    fun `sample for more than one same number`() {
+         day.getAllNumbers("eighttkbtzjz6nineeight") shouldBe "8698"
     }
+
+    @Test
+    fun `map spelled out numbers to int`() {
+        day.allNumbers.map {
+            day.stringToNumber(it)
+        } shouldBe listOf(1,2,3,4,5,6,7,8,9)
+    }
+
+    @Test
+    fun `get all numbers`() {
+        day.getAllNumbers(input[0]) shouldBe "219"
+        day.getAllNumbers(input[1]) shouldBe "823"
+        day.getAllNumbers(input[2]) shouldBe "123"
+        day.getAllNumbers(input[3]) shouldBe "2134"
+        day.getAllNumbers(input[4]) shouldBe "49872"
+        day.getAllNumbers(input[5]) shouldBe "18234"
+        day.getAllNumbers(input[6]) shouldBe "76"
+    }
+    @Test
+    fun `get calibration value for line`() {
+         day.getCalibrationValue(input[0]) shouldBe 29
+         day.getCalibrationValue(input[1]) shouldBe 83
+         day.getCalibrationValue(input[2]) shouldBe 13
+         day.getCalibrationValue(input[3]) shouldBe 24
+         day.getCalibrationValue(input[4]) shouldBe 42
+         day.getCalibrationValue(input[5]) shouldBe 14
+         day.getCalibrationValue(input[6]) shouldBe 76
+    }
+
+    @Test
+    fun `sum of calculation values`() {
+         day.part2(input) shouldBe 281
+    }
+
 }
