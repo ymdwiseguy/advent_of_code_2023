c1a02069e7ecddaed34b8e755d38e5c1d8c3b409
Matthias Diekmann <9092617+ymdwiseguy@users.noreply.github.com>
2023-12-06 15:30:51 +0100

    solve day 3

--- src/main/day03/Day03.kt
+++ src/main/day03/Day03.kt
@@ -11,7 +11,113 @@ class Day03 {
     val input = readInput("resources/day03/data")
     val testInput = readInput("resources/day03/data_test")
 
-    fun part1(input: List<String>): Int = input.size
+    fun part1(input: List<String>): Int {
+        val symbols = findSymbols(input)
+        val numbers = findNumbers(input)
 
-    fun part2(input: List<String>): Int = input.size
+        var sum = 0
+        numbers.forEach { myNumber ->
+            if (symbols.map(Symbol::coordinate).any { it in myNumber.neighbours() }) {
+                sum += myNumber.number.toInt()
+            }
+        }
+        return sum
+    }
+
+    fun part2(input: List<String>): Long = findGears(findSymbols(input), findNumbers(input)).sumOf {
+        it.adjacentNumbers.first().number.toLong() * it.adjacentNumbers.last().number.toLong()
+    }
+
+    fun findNumbers(input: List<String>): List<MyNumber> = input.flatMapIndexed { indexY: Int, row: String ->
+        var indexX = 1
+        val numbers = mutableListOf<MyNumber>()
+        row.mapNotNull { char ->
+            if (char.isDigit()) {
+                when {
+                    numbers.isEmpty() -> numbers.add(
+                        MyNumber(indexX..indexX, indexY + 1, char.toString())
+                    )
+
+                    numbers.last().x.last == indexX - 1 -> {
+                        val lastNumber = numbers.last()
+                        numbers.remove(lastNumber)
+                        numbers.add(
+                            lastNumber.copy(
+                                lastNumber.x.first..indexX, indexY + 1, lastNumber.number + char
+                            )
+                        )
+                    }
+
+                    else -> numbers.add(
+                        MyNumber(indexX..indexX, indexY + 1, char.toString())
+                    )
+                }
+            }
+            indexX++
+        }
+        numbers
+    }
+
+    fun findSymbols(input: List<String>): List<Symbol> = input.flatMapIndexed { indexY, row ->
+        var indexX = 1
+        row.mapNotNull { char ->
+            val returnValue = if (char.isDigit().not() && char != '.') Symbol(
+                Coordinate(indexX, indexY + 1), char
+            ) else null
+            indexX++
+            returnValue
+        }
+    }
+
+    fun findGears(symbols: List<Symbol>, numbers: List<MyNumber>): List<Symbol> {
+        val potentialGears = symbols.filter { it.char == '*' }
+        val relevantNeighbours = mutableListOf<MyNumber>()
+        numbers.forEach { myNumber ->
+            if (potentialGears.map(Symbol::coordinate).any { it in myNumber.neighbours() }) {
+                relevantNeighbours.add(myNumber)
+            }
+        }
+
+        potentialGears.forEach { symbol ->
+            relevantNeighbours.forEach { myNumber ->
+                if (symbol.coordinate in myNumber.neighbours()) {
+                    symbol.adjacentNumbers.add(myNumber)
+                }
+            }
+        }
+
+        return potentialGears.filter { it.adjacentNumbers.size == 2 }
+    }
 }
+
+data class Coordinate(
+    val x: Int,
+    val y: Int,
+)
+
+data class MyNumber(
+    val x: IntRange,
+    val y: Int,
+    val number: String,
+) {
+    fun neighbours(): List<Coordinate> = listOf(
+        Coordinate(x.first - 1, y - 1),
+        Coordinate(x.first - 1, y),
+        Coordinate(x.first - 1, y + 1),
+    ) + x.flatMap { xValue: Int ->
+        listOf(
+            Coordinate(xValue, y - 1),
+            Coordinate(xValue, y + 1),
+        )
+    } + listOf(
+        Coordinate(x.last + 1, y - 1),
+        Coordinate(x.last + 1, y),
+        Coordinate(x.last + 1, y + 1),
+    )
+}
+
+data class Symbol(
+    val coordinate: Coordinate,
+    val char: Char,
+    val adjacentNumbers: MutableSet<MyNumber> = mutableSetOf()
+)
--- src/test/day03/Day03_test.kt
+++ src/test/day03/Day03_test.kt
@@ -15,7 +15,84 @@ class Day03Test {
     }
 
     @Test
-    fun `failing dummy test`() {
-         input.size shouldBe 1
+    fun `solve part 2`() {
+         day.part2(input) shouldBe 467835L
+         day.part2(day.input) shouldBe 91622824L
+    }
+
+    @Test
+    fun `find gears`() {
+        val symbols = day.findSymbols(input)
+        val numbers = day.findNumbers(input)
+         day.findGears(symbols, numbers) shouldBe listOf(
+             Symbol(
+                 coordinate = Coordinate(x = 4, y = 2),
+                 char = '*',
+                 adjacentNumbers = mutableSetOf(
+                     MyNumber(1..3,1,"467"),
+                     MyNumber(3..4,3,"35"),
+                 )
+             ),
+             Symbol(
+                 coordinate = Coordinate(x = 6, y = 9),
+                 char = '*',
+                 adjacentNumbers = mutableSetOf(
+                     MyNumber(7..9,8,"755"),
+                     MyNumber(6..8,10,"598"),
+                 )
+             ),
+         )
+    }
+
+    @Test
+    fun `solve part 1`() {
+         day.part1(input) shouldBe 4361
+         day.part1(day.input) shouldBe 560670
+    }
+
+    @Test
+    fun `get neighbours`() {
+        MyNumber(1..3,1,"467").neighbours() shouldBe listOf(
+            Coordinate(0,0),
+            Coordinate(0,1),
+            Coordinate(0,2),
+            Coordinate(1,0),
+            Coordinate(1,2),
+            Coordinate(2,0),
+            Coordinate(2,2),
+            Coordinate(3,0),
+            Coordinate(3,2),
+            Coordinate(4,0),
+            Coordinate(4,1),
+            Coordinate(4,2),
+        )
+    }
+
+    @Test
+    fun `find symbols with coordinates`() {
+         day.findSymbols(input) shouldBe listOf(
+             Symbol(Coordinate(4, 2), '*'),
+             Symbol(Coordinate(7, 4), '#'),
+             Symbol(Coordinate(4, 5), '*'),
+             Symbol(Coordinate(6, 6), '+'),
+             Symbol(Coordinate(4, 9), '$'),
+             Symbol(Coordinate(6, 9), '*'),
+         )
+    }
+
+    @Test
+    fun `find numbers with coordinates`() {
+         day.findNumbers(input) shouldBe listOf(
+             MyNumber(1..3,1,"467"),
+             MyNumber(6..8,1,"114"),
+             MyNumber(3..4,3,"35"),
+             MyNumber(7..9,3,"633"),
+             MyNumber(1..3,5,"617"),
+             MyNumber(8..9,6,"58"),
+             MyNumber(3..5,7,"592"),
+             MyNumber(7..9,8,"755"),
+             MyNumber(2..4,10,"664"),
+             MyNumber(6..8,10,"598"),
+         )
     }
 }
