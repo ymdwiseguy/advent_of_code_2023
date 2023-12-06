0f18f3c6cd93db18794582e74ff218d0f0d89576
Matthias Diekmann <9092617+ymdwiseguy@users.noreply.github.com>
2023-12-06 22:13:25 +0100

    solve day 4, part 1

--- src/main/day04/Day04.kt
+++ src/main/day04/Day04.kt
@@ -11,7 +11,40 @@ class Day04 {
     val input = readInput("resources/day04/data")
     val testInput = readInput("resources/day04/data_test")
 
-    fun part1(input: List<String>): Int = input.size
+    fun part1(input: List<String>): Long = input.map(::parseInput).sumOf { it.value() }
 
     fun part2(input: List<String>): Int = input.size
+
+    fun parseInput(s: String): Card =
+        s.split("Card ", ": ", " | ").let { parts ->
+            Card(
+                id = parts[1],
+                winners = parts[2].split(" ").mapNotNull { it.toIntOrNull() },
+                numbers = parts[3].split(" ").mapNotNull { it.toIntOrNull() },
+            )
+        }
 }
+
+data class Card(
+    val id: String,
+    val winners: List<Int>,
+    val numbers: List<Int>,
+) {
+    fun value(): Long {
+        var winnerCount = 0
+        numbers.forEach { number ->
+            if (number in winners) {
+                winnerCount++
+            }
+        }
+        return when {
+            winnerCount == 1 -> 1
+            winnerCount > 1 -> 2.power(winnerCount - 1)
+            else -> 0
+        }
+    }
+}
+
+private fun Int.power(exponent: Int): Long = if (exponent != 0) {
+    this * this.power(exponent - 1)
+} else 1
--- src/test/day04/Day04_test.kt
+++ src/test/day04/Day04_test.kt
@@ -15,7 +15,26 @@ class Day04Test {
     }
 
     @Test
-    fun `failing dummy test`() {
-         input.size shouldBe 1
+    fun `solve part 1`() {
+         day.part1(input) shouldBe 13L
+         day.part1(day.input) shouldBe 23678L
+    }
+
+    @Test
+    fun `parse input`() {
+        day.parseInput("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53") shouldBe CARD_1
+    }
+
+    @Test
+    fun `calculate card value`() {
+         CARD_1.value() shouldBe 8L
+    }
+
+    private companion object {
+        val CARD_1 = Card(
+            id = "1",
+            winners = listOf(41, 48, 83, 86, 17),
+            numbers = listOf(83, 86, 6, 31, 17, 9, 48, 53),
+        )
     }
 }
